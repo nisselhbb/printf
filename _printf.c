9ca2cc2d31652cb1749943702b552e9e37e9b3ee
nisselhbb <nissrineelhobbi@gmail.com>
2023-07-24 15:00:52 -0700

    function that produces output according to a format _printf

---
+++ _printf.c
@@ -0,0 +1,56 @@
+#include "main.h"
+
+/**
+ * _printf - a function that produces
+ * output according to a format
+ * @format: pointer to the format
+ * Return: number of characters
+ */
+int _printf(const char *format, ...)
+{
+	va_list args;
+	va_start (args, format);
+
+	int num = 0;
+
+	while (*format != NULL)
+	{
+		if (*format == '%')
+			format++;
+
+	char specifier = *format;
+	const char *string = va_arg(args, const char*);
+
+	switch (specifier)
+	{
+		case 'c':
+			_putchar(va_arg(args, int));
+			num++;
+			break;
+		case 's':
+			while (*string != NULL)
+			{
+				_putchar(*string);
+				num++;
+				string++;
+			}
+			break;
+		case '%':
+			_putchar('%');
+			num++;
+			break;
+		default:
+			_putchar('%');
+			_putchar(specifier);
+			num = num + 2;
+	}
+	else
+	{
+		_putchar(*format);
+		num++;
+	}
+	format++;
+	}
+	va_end(args);
+	return (num);
+}
---
+++ main.h
@@ -0,0 +1,20 @@
+#ifndef PRINTF_H
+#define PRINT_H
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+int _putchar(char c);
+int _printf(const char *format, ...);
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+    return (write(1, &c, 1));
+}
+#endif
