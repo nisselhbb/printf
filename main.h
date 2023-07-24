cec70d03993698872fb22f7006cc4ce3558e102d
nisselhbb <nissrineelhobbi@gmail.com>
2023-07-24 16:05:37 -0700

    function that produces output according to a format _printf

--- main.h
+++ main.h
@@ -6,6 +6,6 @@
 #include <unistd.h>
 int _putchar(char c);
 int _printf(const char *format, ...);
-int print_special_string(char *string);
+int print_special_string(char *str);
 int print_char_string_or_percent(char specifier, va_list args);
 #endif
