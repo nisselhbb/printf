3405ce52b9aa6353b3cff360934aee72696d5610
nisselhbb <nissrineelhobbi@gmail.com>
2023-07-24 16:46:37 -0700

    function that produces output according to a format _printf

--- main.h
+++ main.h
@@ -29,4 +29,36 @@ int print_char_string_or_percent(char specifier, va_list args)
 			return (_putchar('%') + _putchar(specifier));
 	}
 }
+#include "main.h"
+/**
+ * print_char - prints a character to
+ * stdout
+ * @args: printed arguments
+ * Return: number of characters
+ */
+int print_char(va_list args)
+{
+	char c = va_arg(args, int);
+
+	return (_putchar (c));
+}
+/**
+ * print_string - prints a string
+ * @args: printed arguments
+ * Return: number of characters
+ */
+int print_string(va_list args)
+{
+	int num = 0;
+	char *str = va_arg(args, char *);
+
+	if (str == NULL)
+		str = "(null)";
+	while (*str)
+	{
+		num = num + _putchar (*str);
+		str++;
+	}
+	return (num);
+}
 #endif
