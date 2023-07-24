0d14a26b3a2b2aeb583c79f14f09bdf3d774f50d
nisselhbb <nissrineelhobbi@gmail.com>
2023-07-24 16:38:09 -0700

    function that produces output according to a format _printf

--- main.h
+++ main.h
@@ -8,4 +8,25 @@ int _putchar(char c);
 int _printf(const char *format, ...);
 int print_special_string(char *str);
 int print_char_string_or_percent(char specifier, va_list args);
+/**
+ * print_char_string_or_percent - prints a character, string
+ * or percent symbol
+ * @specifier: conversion specifier
+ * @args: printed arguments
+ * Return: number of characters
+ */
+int print_char_string_or_percent(char specifier, va_list args)
+{
+        switch (specifier)
+        {
+                case 'c':
+                        return (print_char(args));
+                case 's':
+                        return (print_string(args));
+                case '%':
+                        return (_putchar('%'));
+        default:
+        return (_putchar('%') + _putchar(specifier));
+        }
+}
 #endif
--- print_functions.c
+++ print_functions.c
@@ -29,24 +29,4 @@ int print_string(va_list args)
 		str++;
 	}
 	return (num);
-/**
- * print_char_string_or_percent - prints a character, string
- * or percent symbol
- * @specifier: conversion specifier
- * @args: printed arguments
- * Return: number of characters
- */
-int print_char_sttring_or_percent(char specifier, va_list args)
-{
-	switch (specifier)
-	{
-		case 'c':
-			return (print_char(args));
-		case 's':
-			return (print_string(args));
-		case '%':
-			return (_putchar('%'));
-	}
-	default:
-	return (_putchar('%') + _putchar(specifier));
 }
