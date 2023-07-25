26b3290cb55228bf201cf9c57eac17266f4be712
delta-az <azedinedelta@gmail.com>
2023-07-25 10:24:03 -0700

    r

--- _printf.c
+++ _printf.c
@@ -1,17 +1,18 @@
 #include "main.h"
+
 void print_buffer(char buffer[], int *buff_ind);
+
 /**
- * _printf - function that produces output
- * according to a format
- * @format: a character string
- * Return: the number of character
+ * _printf - produces output according to a format
+ * @format: character string
+ * Return: number of characters printed
  */
 int _printf(const char *format, ...)
 {
 	int i, printed = 0, printed_chars = 0;
-	int width, buff_ind = 0;
-	char buffer[BUFF_SIZE];
+	int flags, width, precision, size, buff_ind = 0;
 	va_list list;
+	char buffer[BUFF_SIZE];
 
 	if (format == NULL)
 		return (-1);
@@ -21,49 +22,38 @@ int _printf(const char *format, ...)
 	{
 		if (format[i] != '%')
 		{
-			buffer[buff_ind] = format[i];
+			buffer[buff_ind++] = format[i];
 			if (buff_ind == BUFF_SIZE)
 				print_buffer(buffer, &buff_ind);
 			printed_chars++;
 		}
 		else
 		{
-			if (format[i + 1] == '%')
-			{
-				buffer[buff_ind++] = '%';
-				i++;
-				if (buff_ind == BURR_SIZE)
-					print_buffer(buffer, buff_ind);
-				printed_chars++;
-			}
-			else if (format[i + 1] == 'c')
-			{
-				width = va_arg(list, int);
-				printed = print_char(list, buffer, width);
-				if (printed == -1)
-					return (-1);
-				print_chars += printed;
-				i++;
-			}
-			else if (format[i + 1] == 's')
-			{
-				width = va_arg(list, int);
-				printed = printe_string(list, buffer, width);
-				if (printed == -1)
-					return (-1);
-				print_chasr += printed;
-				i++;
-			}
-			else
-			{
-				buffer[buff_ind++] = format[i];
-				if (buff_ind == BUFF_SIZE)
-					print_buffer(buffer, &buff_ind);
-				printed_chars++;
-			}
+			print_buffer(buffer, &buff_ind);
+			flags = get_flags(format, &i);
+			width = get_width(format, &i, list);
+			precision = get_precision(format, &i, list);
+			size = get_size(format, &i);
+			++i;
+			printed = handle_print(format, &i, list, buffer,
+					flags, width, precision, size);
+			if (printed == -1)
+				return (-1);
+			printed_chars += printed;
 		}
 	}
 	print_buffer(buffer, &buff_ind);
 	va_end(list);
 	return (printed_chars);
+}
+/**
+ * print_buffer - Prints the contents of the buffer if it exist
+ * @buffer: Array
+ * @buff_ind: Index
+ */
+void print_buffer(char buffer[], int *buff_ind)
+{
+	if (*buff_ind > 0)
+		write(1, &buffer[0], *buff_ind);
+	*buff_ind = 0;
 }
