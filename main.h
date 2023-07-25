7ab6810f3c227ef3151d0d1493ee238ad00fd530
nisselhbb <nissrineelhobbi@gmail.com>
2023-07-24 17:01:48 -0700

    function that produces output according to a format _printf

--- main.h
+++ main.h
@@ -6,59 +6,6 @@
 #include <unistd.h>
 int _putchar(char c);
 int _printf(const char *format, ...);
-int print_special_string(char *str);
 int print_char_string_or_percent(char specifier, va_list args);
-/**
- * print_char_string_or_percent - prints a character, string
- * or percent symbol
- * @specifier: conversion specifier
- * @args: printed arguments
- * Return: number of characters
- */
-int print_char_string_or_percent(char specifier, va_list args)
-{
-	switch (specifier)
-	{
-		case 'c':
-			return (print_char(args));
-		case 's':
-			return (print_string(args));
-		case '%':
-			return (_putchar('%'));
-		default:
-			return (_putchar('%') + _putchar(specifier));
-	}
-}
-#include "main.h"
-/**
- * print_char - prints a character to
- * stdout
- * @args: printed arguments
- * Return: number of characters
- */
-int print_char(va_list args)
-{
-	char c = va_arg(args, int);
-
-	return (_putchar (c));
-}
-/**
- * print_string - prints a string
- * @args: printed arguments
- * Return: number of characters
- */
-int print_string(va_list args)
-{
-	int num = 0;
-	char *str = va_arg(args, char *);
-
-	if (str == NULL)
-		str = "(null)";
-	while (*str)
-	{
-		num = num + _putchar (*str);
-		str++;
-	}
-	return (num);
-}
+int print_special_string(char *str);
 #endif
